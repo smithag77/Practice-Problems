1c75e937a9d33f204975ced4a091129b7b5bc580
smithag77 <agsmith@gmail.com>
2017-11-03 13:21:11 -0500

    13 and 14

---
+++ Codility/14/MinMaxDivision.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+
+namespace Codility._14
+{
+    public class MinMaxDivision
+    {
+        public int solution(int K, int M, int[] A)
+        {
+            long sumLowerBound = A.Max();
+            long sumUpperBound = A.Sum();
+
+            long result = 0;
+            // binary search in the large sums testing for the validity of the solution
+            while (sumLowerBound <= sumUpperBound)
+            {
+                var midVal = (sumLowerBound + sumUpperBound) / 2;
+                var isValid = IsValidSolution(A, midVal, K);
+
+                if (isValid)
+                {
+                    sumUpperBound = midVal - 1;
+                    result = midVal;
+                }
+                else
+                {
+                    sumLowerBound = midVal + 1;
+                }
+            }
+
+            return (int)result;
+        }
+
+        private static bool IsValidSolution(int[] A, long M, int K)
+        {
+            var partialSum = A[0];
+            var intervals = 0;
+            for (var i = 1; i < A.Length; i++)
+            {
+                if (partialSum + A[i] > M)
+                {
+                    partialSum = A[i];
+                    intervals++;
+                }
+                else
+                {
+                    partialSum += A[i];
+                }
+
+                if (intervals >= K)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Codility/14/NailingPlanks.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Codility._14
+{
+    class NailingPlanks
+    {
+        public int solution(int[] A, int[] B, int[] C)
+        {
+            var sortedNails = GetSortedNails(C);
+            var result = -1;
+
+            for (var i = 0; i < A.Length; i++)
+            {
+                result = FindFirstNailIndex(A[i], B[i], sortedNails, result);
+
+                if (result == -1)
+                {
+                    return -1;
+                }
+            }
+
+            return result + 1;
+        }
+
+        private int FindFirstNailIndex(int plankBegin, int plankEnd, IList<Nail> nailsInOrder, int preResult)
+        {
+            var firstNailOrder = GetFirstNailOrder(plankBegin, plankEnd, nailsInOrder);
+
+            // Can't find a nail that could nail the plank
+            if (firstNailOrder == -1)
+            {
+                return -1;
+            }
+
+            var bestNailIdx = FindBestNailForPlank(plankEnd, firstNailOrder, nailsInOrder, preResult);
+
+            return bestNailIdx;
+        }
+
+        private int GetFirstNailOrder(int plankBegin, int plankEnd, IList<Nail> nailsInOrder)
+        {
+            var firstNailOrder = -1;
+
+            var nailOrderLower = 0;
+            var nailOrderUpper = nailsInOrder.Count - 1;
+            var nailOrderMid = 0;
+
+            // Search by position :: Find the first nail
+            // which is between plankBegin and plankEnd
+            // (i.e. has the lowest acceptable position)
+            while (nailOrderLower <= nailOrderUpper)
+            {
+                nailOrderMid = (nailOrderLower + nailOrderUpper) / 2;
+                var positionMid = nailsInOrder[nailOrderMid].Position;
+
+                if (positionMid < plankBegin)
+                {
+                    nailOrderLower = nailOrderMid + 1;
+                }
+                else
+                {
+                    nailOrderUpper = nailOrderMid - 1;
+
+                    if (positionMid <= plankEnd)
+                    {
+                        firstNailOrder = nailOrderMid;
+                    }
+                }
+            }
+
+            return firstNailOrder;
+        }
+
+        private int FindBestNailForPlank(int plankEnd, int firstNailOrder, IList<Nail> nailsInOrder, int preResult)
+        {
+            var bestNailIdx = nailsInOrder[firstNailOrder].Index;
+
+            // Search by index :: Linearly search all qualifying
+            // nails and find the one that is used the earliest
+            // (i.e. has the lowest index)
+            var nailOrder = firstNailOrder + 1;
+            while (nailOrder < nailsInOrder.Count)
+            {
+                if (nailsInOrder[nailOrder].Position > plankEnd)
+                {
+                    break;
+                }
+
+                bestNailIdx = Math.Min(bestNailIdx, nailsInOrder[nailOrder].Index);
+
+                nailOrder++;
+
+                // The current result won't influence the overall result
+                // because some other plank needs a nail used later than
+                // the best nail for the current one
+                if (preResult >= bestNailIdx)
+                {
+                    return preResult;
+                }
+            }
+
+            return Math.Max(bestNailIdx, preResult);
+        }
+
+        private IList<Nail> GetSortedNails(int[] nails)
+        {
+            var nailIndex = new int[nails.Length];
+
+            for (var i = 0; i < nails.Length; i++)
+            {
+                nailIndex[i] = i;
+            }
+
+            var nailCompare = Comparer<int>.Create(new Comparison<int>((x, y) =>
+            {
+                if (nails[x] == nails[y])
+                {
+                    return x - y;
+                }
+                return nails[x] - nails[y];
+            }));
+
+            Array.Sort(nailIndex, nailCompare);
+            var sortedNails = nailIndex.Select(idx => new Nail { Index = idx, Position = nails[idx] }).ToList();
+
+            return sortedNails;
+        }
+
+        private struct Nail
+        {
+            public int Index;
+            public int Position;
+        }
+    }
+}
